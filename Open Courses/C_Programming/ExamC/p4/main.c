d313e4085d68d753fe1a623deb6e36e454e54949
Kosta Dinkov <k.v.dinkov@gmail.com>
2015-12-12 16:58:14 +0200

    C Exam Update

---
+++ Open Courses/C_Programming/ExamC/p4/main.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Prototypes
+int GetInt(void);
+unsigned long long GetLongLong(void);
+char* GetString(void);
+
+
+int main()
+{
+	return 0;
+}
+
+
+int GetInt(void)
+{
+	while (1)
+	{
+		char* line = GetString();
+		if (line == NULL)
+		{
+			return INT_MAX;
+		}
+
+		int n;
+		char c;
+		if (sscanf(line, " %d %c", &n, &c) == 1)
+		{
+			free(line);
+			return n;
+		}
+		else
+		{
+			free(line);
+			printf("Retry: ");
+		}
+	}
+}
+
+unsigned long long GetLongLong(void)
+{
+	while (1)
+	{
+		char* line = GetString();
+		if (line == NULL)
+		{
+			return ULLONG_MAX;
+		}
+
+		unsigned long long n;
+		char c;
+		if (sscanf(line, " %llu %c", &n, &c) == 1)
+		{
+			free(line);
+			return n;
+		}
+		else
+		{
+			free(line);
+			printf("Retry: ");
+		}
+	}
+}
+
+char* GetString(void)
+{
+	char* buffer = NULL;
+
+	unsigned int capacity = 0;
+
+	unsigned int n = 0;
+
+	int c;
+
+	while ((c = fgetc(stdin)) != '\n' && c != EOF)
+	{
+		if (n + 1 > capacity)
+		{
+			if (capacity == 0)
+			{
+				capacity = 32;
+			}
+			else if (capacity <= (UINT_MAX / 2))
+			{
+				capacity *= 2;
+			}
+			else
+			{
+				free(buffer);
+				return NULL;
+			}
+
+			char* temp = (char *)realloc(buffer, capacity * sizeof(char));
+			if (temp == NULL)
+			{
+				free(buffer);
+				return NULL;
+			}
+			buffer = temp;
+		}
+
+		buffer[n++] = c;
+	}
+
+	if (n == 0 && c == EOF)
+	{
+		return NULL;
+	}
+
+	char* minimal = (char *)malloc((n + 1) * sizeof(char));
+	strncpy(minimal, buffer, n);
+	free(buffer);
+
+	minimal[n] = '\0';
+
+	return minimal;
+}
+
+
