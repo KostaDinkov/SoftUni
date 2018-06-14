9b93fcdeee114ac94161627783efa6f4132bcb42
Kosta <k.v.dinkov@gmail.com>
2018-06-15 00:32:10 +0300

    add: Programming Fundamentals Objects exercise (additional files)

--- Tech/Fundamentals/Objects/Objects/_09.TeamworkProjects/TeamWorkProjects.cs
+++ Tech/Fundamentals/Objects/Objects/_09.TeamworkProjects/TeamWorkProjects.cs
@@ -1,22 +1,21 @@
-﻿using System;
-
-namespace _09.TeamworkProjects
+﻿namespace _09.TeamworkProjects
 {
+    using System;
     using System.Collections.Generic;
-    using System.IO;
+    using System.Linq;
 
-
-    class TeamWorkProjects
+    internal class TeamWorkProjects
     {
-        static void Main()
+        private static void Main()
         {
-            Console.SetIn(new StreamReader("input.txt"));
-            var teams = new Dictionary<string,Team>();
+            //Console.SetIn(new StreamReader("input.txt"));
+            var teams = new Dictionary<string, Team>();
             var creators = new HashSet<string>();
-            
+            var usersInTeams = new HashSet<string>();
+
             //Read teams
-            int teamCount = int.Parse(Console.ReadLine());
-            for (int i = 0; i < teamCount; i++)
+            var teamCount = int.Parse(Console.ReadLine());
+            for (var i = 0; i < teamCount; i++)
             {
                 var inputTokens = Console.ReadLine().Split("-");
                 var creator = inputTokens[0].Trim();
@@ -25,26 +24,26 @@ namespace _09.TeamworkProjects
                 if (teams.ContainsKey(teamName))
                 {
                     Console.WriteLine($"Team {teamName} was already created!");
-                    break;
+                    continue;
                 }
-                else if (creators.Contains(creator))
+
+                if (creators.Contains(creator))
                 {
                     Console.WriteLine($"{creator} cannot create another team!");
-                    break;
+                    continue;
                 }
-                teams.Add(teamName, new Team(teamName,creator));
+
+
+                teams.Add(teamName, new Team(teamName, creator));
+                creators.Add(creator);
                 Console.WriteLine($"Team {teamName} has been created by {creator}!");
-                
             }
-            
+
             //Assign members
             while (true)
             {
                 var inputLine = Console.ReadLine();
-                if (inputLine == "end of assignment")
-                {
-                    break;
-                }
+                if (inputLine == "end of assignment") break;
 
                 var inputTokens = inputLine.Split("->");
                 var user = inputTokens[0].Trim();
@@ -53,22 +52,41 @@ namespace _09.TeamworkProjects
                 if (!teams.ContainsKey(team))
                 {
                     Console.WriteLine($"Team {team} does not exist!");
-                    break;
+                    continue;
                 }
 
-                if (!teams[team].AddMember(user))
+                if (usersInTeams.Contains(user) || creators.Contains(user))
                 {
                     Console.WriteLine($"Member {user} cannot join team {team}!");
-                    break;
+                    continue;
                 }
+
+                teams[team].Members.Add(user);
+                usersInTeams.Add(user);
             }
-            
+
             //Print results
-            
+            var validTeams = teams
+                .Where(t => t.Value.Members.Count > 0)
+                .OrderByDescending(t => t.Value.Members.Count)
+                .ThenBy(t => t.Key);
+
+            var disbandTeams = teams.Where(t => t.Value.Members.Count == 0).OrderBy(t => t.Key);
+
+            foreach (var teamPair in validTeams)
+            {
+                Console.WriteLine(teamPair.Key);
+                Console.WriteLine($"- {teamPair.Value.Creator}");
+                var membersSorted = teamPair.Value.Members.OrderBy(m => m);
+                foreach (var member in membersSorted) Console.WriteLine($"-- {member}");
+            }
+
+            Console.WriteLine("Teams to disband:");
+            foreach (var teamPair in disbandTeams) Console.WriteLine(teamPair.Key);
         }
     }
 
-    class Team
+    internal class Team
     {
         public Team(string name, string creator)
         {
@@ -76,21 +94,9 @@ namespace _09.TeamworkProjects
             this.Creator = creator;
             this.Members = new List<string>();
         }
-        
+
         public string Name { get; set; }
         public List<string> Members { get; set; }
         public string Creator { get; set; }
-
-        public bool AddMember(string username)
-        {
-            if (this.Members.Contains(username) || this.Creator == username)
-            {
-                return false;
-            }
-            this.Members.Add(username);
-            return true;
-        }
-        
-        
     }
 }
