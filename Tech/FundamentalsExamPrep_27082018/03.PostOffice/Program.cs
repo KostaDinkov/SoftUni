b46e73d22c4f9a29102af9eb6414140f662d11b0
Kosta <k.v.dinkov@gmail.com>
2018-10-26 14:30:26 +0300

    Update: tech fundamentals exam prep

--- Tech/FundamentalsExamPrep_27082018/03.PostOffice/Program.cs
+++ Tech/FundamentalsExamPrep_27082018/03.PostOffice/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _03.PostOffice
 {
@@ -6,7 +9,34 @@ namespace _03.PostOffice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine().Split('|');
+
+            var capitalLetters = Regex.Match(input[0], @".*([#$%*&])([A-Z]+)(?:\1)").Groups[2];
+           
+
+            var uniqueCodes = new HashSet<string>();
+            var matches = Regex.Matches(input[1], @"([6][5-9]|[7][0-9]|[8][0-9]|90):([0-1][1-9]|20)");
+            foreach (Match match in matches)
+            {
+                uniqueCodes.Add(match.Value);
+            }
+
+            var results = new string[capitalLetters.Length];
+
+            foreach (string uniqueCode in uniqueCodes)
+            {
+                var parameters = uniqueCode.Split(':').ToArray();
+                var letter = (char) int.Parse(parameters[0]);
+                var length = int.Parse(parameters[1]);
+                var pattern = @"(?: |^)(" + letter + @"[^ ]{" + length + @"})(?: |$)";
+                string match = Regex.Match(input[2],pattern).Groups[1].Value;
+                results[capitalLetters.Value.IndexOf(letter)]=(match);
+            }
+
+            Console.WriteLine(string.Join('\n',results));
+
+
+
         }
     }
 }
---
+++ Tech/FundamentalsExamPrep_27082018/04.IronGirder/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml;
+
+namespace _04.IronGirder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new Dictionary<string, Stats>();
+            var input = Console.ReadLine();
+            while (input != "Slide rule")
+            {
+                var(city,time,count) = ParseInput(input);
+
+                AddEntry(city, time, count, cities);
+
+                input = Console.ReadLine();
+            }
+
+            var sorted = cities.Where(p=>p.Value.BestTime>0 && p.Value.TotalPassengers>0).OrderBy(p => p.Value.BestTime).ThenBy(p => p.Key);
+            foreach (var city in sorted)
+            {
+                Console.WriteLine($"{city.Key} -> Time: {city.Value.BestTime} -> Passengers: {city.Value.TotalPassengers}");
+            }
+        }
+
+        private static void AddEntry(string city, int time, int count, Dictionary<string, Stats> cities)
+        {
+            if (cities.ContainsKey(city))
+            {
+                //ambush
+                if (time == 0)
+                {
+                    cities[city].BestTime = 0;
+                }
+                else if (time < cities[city].BestTime || cities[city].BestTime ==0 ) 
+                {
+                    cities[city].BestTime = time;
+                }
+
+                cities[city].TotalPassengers += count;
+                if (cities[city].TotalPassengers < 0)
+                {
+                    cities[city].TotalPassengers = 0;
+                }
+            }
+            else
+            {
+                
+                cities.Add(city, new Stats(){BestTime = time, TotalPassengers = count>0?count:0});
+                
+            }
+        }
+
+        private static (string , int, int ) ParseInput(string input)
+        {
+            var (city,stats ) = input.Split(":");
+            var (timeStr, (countStr,rest)) = stats[0].Split("->");
+            
+            var isAmbushed = int.TryParse(timeStr, out var time);
+
+            var count = isAmbushed ? int.Parse(countStr) : -int.Parse(countStr);
+
+            return (city, time, count);
+        }
+    }
+
+    class Stats
+    {
+        public int BestTime;
+        public int TotalPassengers;
+    }
+
+    public static class ArrayExtensions
+    {
+        public static void Deconstruct<T>(this T[] array, out T first, out T[] rest )
+        {
+            first = array.Length > 0 ? array[0] : default(T);
+            rest = array.Skip(1).ToArray();
+        }
+    }
+}
