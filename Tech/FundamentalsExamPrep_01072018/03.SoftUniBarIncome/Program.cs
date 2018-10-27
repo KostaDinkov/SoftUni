b90a194ef05e38e9ad9ed8de229f866e925824b5
Kosta <k.v.dinkov@gmail.com>
2018-10-27 11:12:21 +0300

    Update: tech fundamentals exam prep 03.SoftUni Bar Income - 100/100

---
+++ Tech/FundamentalsExamPrep_01072018/03.SoftUniBarIncome/Program.cs
@@ -0,0 +1,40 @@
+﻿//#define ReadFromFile
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace _03.SoftUniBarIncome
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+#if ReadFromFile
+            Console.SetIn(new StreamReader("..\\..\\..\\test1.txt"));
+#endif
+            var totalIncome = 0d;
+            var inputLine = Console.ReadLine();
+            var pattern = new Regex(@"(?:%([A-Z]{1}[a-z]+)%).*?(?:<(\w+)>).*?(?:\|(\d+)\|).*?(?:(\d+(?:\.\d+)*)\$)");
+
+            while (inputLine != "end of shift")
+            {
+                var parameters = pattern.Match(inputLine).Groups;
+                if (parameters.Count == 5)
+                {
+                    var name = parameters[1].Value;
+                    var product = parameters[2].Value;
+                    var productCount = int.Parse(parameters[3].Value);
+                    var price = double.Parse(parameters[4].Value);
+
+                    totalIncome += productCount * price;
+
+                    Console.WriteLine($"{name}: {product} - {Math.Round(productCount * price, 2):F2}");
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total income: {Math.Round(totalIncome, 2):F2}");
+        }
+    }
+}
