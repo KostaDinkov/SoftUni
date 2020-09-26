c11b8ac1c893ce632a4190acb953e2e5d726665b
Kosta <k.v.dinkov@gmail.com>
2020-09-26 12:28:53 +0300

    add: csweb lecture-1 demo code

---
+++ CSWeb/Basics/01.WebServer-HTTP/HttpClientDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string url = "https://torti-jana.com";
+            HttpClient httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            Console.WriteLine(html);
+        }
+    }
+}
