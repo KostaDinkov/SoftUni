1e10d316a5f3c132b32bfd91dc444a4501f51391
Kosta <k.v.dinkov@gmail.com>
2020-10-15 21:13:00 +0300

    add: sus project

---
+++ CSWeb/Basics/SUS/FirstMvcApps/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http.Headers;
+using SUS.HTTP;
+
+namespace FirstMvcApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHttpServer server = new HttpServer();
+            
+
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/about", About);
+            server.AddRoute("/users/login", Login);
+
+
+            static HttpResponse HomePage( HttpRequest request)
+            {
+                throw new NotImplementedException();
+            }
+            static HttpResponse About(HttpRequest request)
+            {
+                throw new NotImplementedException();
+            }
+            static HttpResponse Login(HttpRequest request)
+            {
+                throw new NotImplementedException();
+            }
+            server.StartAsync(80);
+        }
+    }
+}
---
+++ CSWeb/Basics/SUS/SUS.HTTP/HttpServer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+
+namespace SUS.HTTP
+{
+    public class HttpServer : IHttpServer
+    {
+        IDictionary<string, Func<HttpRequest, HttpResponse>> routeTable = new Dictionary<string, Func<HttpRequest, HttpResponse>>();
+        public void AddRoute(string path, Func<HttpRequest, HttpResponse> action)
+        {
+            if (routeTable.ContainsKey(path))
+            {
+                routeTable[path] = action;
+            }
+            else
+            {
+                routeTable.Add(path, action);
+            }
+
+        }
+
+        public async Task StartAsync(int port)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, port);
+
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                ProcessClientAsync(tcpClient);
+            }
+        }
+
+        private async Task ProcessClientAsync(TcpClient tcpClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
